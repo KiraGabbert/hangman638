a726d09e2b7fbd3350e961fb9d5d6b4632bcc007
Kira <kirinski@hotmail.com>
2024-05-31 18:10:01 +0100

    Milestone_3 updated

--- milestone_3.py
+++ milestone_3.py
@@ -3,4 +3,12 @@ while True:
     if len(guess) == 1 and guess.isalpha() == True:
         break 
     else:
-        print ("Invalid letter. Please, enter a single alphabetical charcter")
+        print ("Invalid letter. Please, enter a single alphabetical character")
+
+import random
+word_list=["lychee", "pear", "cherry", "coconut", "tomato"]
+secret_word = (random.choice(word_list))
+if guess in secret_word:
+    print (f"Good guess! {guess} is in the word.")
+else:
+    print (f"Sorry, {guess} is not in the word Try again.")
