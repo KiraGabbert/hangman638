849b89561150a76b187bd77d168e40bb617f37f2
Kira <kirinski@hotmail.com>
2024-06-01 16:31:45 +0100

    Changes made do _3 file, funcitons added

--- milestone_3.py
+++ milestone_3.py
@@ -1,14 +1,25 @@
-while True:
-    guess=input("Enter a single letter: ")
-    if len(guess) == 1 and guess.isalpha() == True:
-        break 
-    else:
-        print ("Invalid letter. Please, enter a single alphabetical character")
+def ask_for_input():
+    while True:
+        guess=input("Enter a single letter: ")
+        if len(guess) == 1 and guess.isalpha() == True:
+            break 
+        else:
+            print ("Invalid letter. Please, enter a single alphabetical character")
+    check_guess(guess)
+    
 
-import random
-word_list=["lychee", "pear", "cherry", "coconut", "tomato"]
-secret_word = (random.choice(word_list))
-if guess in secret_word:
-    print (f"Good guess! {guess} is in the word.")
-else:
-    print (f"Sorry, {guess} is not in the word Try again.")
+def check_guess(guess):
+    import random
+    word_list=["lychee", "pear", "cherry", "coconut", "tomato"]
+    secret_word = (random.choice(word_list))
+    guess=guess.lower()
+    if guess in secret_word:
+        print (f"Good guess! {guess} is in the word.")
+    else:
+        print (f"Sorry, {guess} is not in the word Try again.")
+
+ask_for_input()
+
+
+
+    
