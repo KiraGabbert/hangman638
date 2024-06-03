366882cd0053fe3fe0b718cdfe37a1729c628c52
Kira <kirinski@hotmail.com>
2024-06-03 12:04:50 +0100

    Latest class construtor and functions updated

--- milestone_4.py
+++ milestone_4.py
@@ -1,8 +1,9 @@
 word_list = ["lychee", "pear", "cherry", "coconut", "tomato"]
 import random
+
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        self.word = list(random.choice(self.word_list))
+        self.word = random.choice(word_list)
         self.word_guessed=["_"]*len(self.word)
         self.num_letters=len(set(self.word))
         self.num_lives=num_lives
@@ -10,7 +11,7 @@ class Hangman:
         self.list_of_guesses=[]
     def check_guess(self, guess):
         guess=guess.lower()
-        if guess in self.word():
+        if guess in self.word:
             print (f"Good guess! {guess} is in the word.")
     def ask_for_input(self):
         while True:
@@ -21,8 +22,10 @@ class Hangman:
                 print ("You already tried that letter!")
             else:
                 self.check_guess(guess)
-            break
-        ask_for_input()
+            
+test = Hangman(word_list)
+test.ask_for_input()
+
 
 
 
