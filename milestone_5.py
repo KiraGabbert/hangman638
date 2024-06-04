d9e2bb8858834d2e751a09c2e81faf211f8eee06
Kira <kirinski@hotmail.com>
2024-06-04 14:20:28 +0100

    Final version of Hangman project code

--- milestone_5.py
+++ milestone_5.py
@@ -2,6 +2,13 @@ word_list = ["apple", "pear", "berry", "grape", "banana"]
 import random
 
 class Hangman:
+    """
+    This Class is used to represent a game.
+    Attributes: 
+        word_list: a list of words from which a secret word is randomly picked;
+        num_lives: a number of wrong guesses a player is allowed to make before the game is over, pre-set to 5.
+    """
+        
     def __init__(self, word_list, num_lives=5):
         self.word = random.choice(word_list)
         self.word_guessed=["_"]*len(self.word)
@@ -9,7 +16,12 @@ class Hangman:
         self.num_lives=num_lives
         self.word_list=word_list
         self.list_of_guesses=[]
+
     def check_guess(self, guess):
+            """ 
+            This function is used to check if a letter guessed by a player is in the secret word.
+            Args: guess: the letter to be compared to letters of the secret word
+            """
         
             if guess in self.word:
                 print (f"Good guess! {guess} is in the word.")
@@ -26,6 +38,10 @@ class Hangman:
             
 
     def ask_for_input(self):
+        """ 
+        This function is used to check if an input by a player is valid, i.e. if it is a single alphabetical character.
+        If a player enters Capital letter the function converts it to lowercase.
+        """
         while True:
             guess=input("Enter a single letter: ")
             guess=guess.lower()
@@ -39,6 +55,9 @@ class Hangman:
             break            
 
 def play_game (word_list):        
+        """ 
+        Here, the function calls the Class to create an instance of the game.
+        """
         game = Hangman (word_list, num_lives=5)    
         while True:
             if game.num_lives == 0:
@@ -51,3 +70,4 @@ def play_game (word_list):
                 break
             
 play_game (word_list)
+
